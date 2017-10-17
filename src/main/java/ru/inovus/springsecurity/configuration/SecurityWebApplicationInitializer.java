fe5fdb6587c0d61f3b4d7a6a53f0bce0e5f5fcf9
vloshkareva <vloshkareva@i-novus.ru>
2017-10-17 11:07:44 +0300

    ауе

---
+++ src/main/java/ru/inovus/springsecurity/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package ru.inovus.springsecurity.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
